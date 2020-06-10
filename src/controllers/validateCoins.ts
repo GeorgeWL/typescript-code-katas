894aed85a0745e095a1634064bdf0d62b50038d6
GeorgeWL <georgelangham1990@gmail.com>
2020-06-10 20:46:27 +0100

    create coinValidation

---
+++ src/controllers/validateCoins.ts
@@ -0,0 +1,39 @@
+import { ICoin, ICoinProperties } from '../interfaces/coinInterface';
+
+export function validateCoins(
+  coins: ICoinProperties[],
+  validCoins: Set<ICoin>
+): boolean[] {
+  return coins.map(coin =>
+    validateCoin(coin, validCoins)
+  );
+}
+
+export function checkValidCoin(
+  coin: ICoinProperties,
+  validCoin: ICoinProperties
+): boolean {
+  return coin.sizeMillimetres === validCoin.sizeMillimetres &&
+    coin.weightGrams === validCoin.weightGrams;
+}
+
+// NOTE: In reality coins would likely have a range of weights - 
+// but for this kata assuming this is perfect world
+export function validateCoin(
+  coin: ICoinProperties,
+  validCoins: Set<ICoin>
+): boolean {
+  // for each valid coin, check if coin is in set
+  // return true if is
+  const validCoinArray = Array.from(validCoins);
+  const isValidCoin = validCoinArray
+    .map((validCoin: ICoin) => {
+      const { sizeMillimetres, weightGrams } = validCoin;
+      const validCoinProperties = { sizeMillimetres, weightGrams };
+      // check that the coin matches any of the valid values
+      return checkValidCoin(coin, validCoinProperties);
+    })
+    // if any are true, regardless of which. return a true
+    .some(val => val === true);
+  return isValidCoin;
+}
