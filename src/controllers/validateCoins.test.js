ea995f1ef490e2f6d7e1837b0902e8438dc3aeef
GeorgeWL <georgelangham1990@gmail.com>
2020-06-10 22:33:55 +0100

    use default coins in validatecoins test

--- src/controllers/validateCoins.test.js
+++ src/controllers/validateCoins.test.js
@@ -1,5 +1,5 @@
 import { validateCoins } from './validateCoins';
-import { DEFAULT_VALID_COINS, CoinPropertiesModel } from '../models/coinModels';
+import { DEFAULT_VALID_COINS } from '../models/coinModels';
 
 describe('Check Valid Coin from Properties', () => {
 
@@ -13,20 +13,15 @@ describe('Coin Validation Test - validateCoin', () => {
 describe('Coin Validation Tests - validateCoins', () => {
   it('should when given only invalid coins, return only false', () => {
     const coins = [
-      new CoinPropertiesModel({ weightGrams: .3, sizeMillimetres: .5 }),
-      new CoinPropertiesModel({ weightGrams: 20, sizeMillimetres: 20 }),
-      new CoinPropertiesModel({ weightGrams: 10 ** 21, sizeMillimetres: 10 ** 21 }), // check big nubmers don't explode it
-      new CoinPropertiesModel({ weightGrams: 10 ** -21, sizeMillimetres: 10 ** -21 }) // check tiny numbers don't explode it
+      { weightGrams: .3, sizeMillimetres: .5 },
+      { weightGrams: 20, sizeMillimetres: 20 },
+      { weightGrams: 10 ** 21, sizeMillimetres: 10 ** 21 }, // check big nubmers don't explode it
+      { weightGrams: 10 ** -21, sizeMillimetres: 10 ** -21 } // check tiny numbers don't explode it
     ];
     expect(validateCoins(coins, DEFAULT_VALID_COINS)).toEqual([false, false, false, false]);
   });
   it('should when given only valid coins, return only true', () => {
-    const coins = [
-      new CoinPropertiesModel({ weightGrams: .3, sizeMillimetres: .5 }),
-      new CoinPropertiesModel({ weightGrams: 20, sizeMillimetres: 20 }),
-      new CoinPropertiesModel({ weightGrams: 10 ** 21, sizeMillimetres: 10 ** 21 }), // check big nubmers don't explode it
-      new CoinPropertiesModel({ weightGrams: 10 ** -21, sizeMillimetres: 10 ** -21 }) // check tiny numbers don't explode it
-    ];
-    expect(validateCoins(coins, DEFAULT_VALID_COINS)).toEqual([false, false, false, false]);
+    const coins = Array.from(DEFAULT_VALID_COINS).map(coin => coin.CoinProperties);
+    expect(validateCoins(coins)).toEqual([true, true, true]);
   });
 });
