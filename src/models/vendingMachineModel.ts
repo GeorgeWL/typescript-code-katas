05450bee1b50143935a39cb7b81978f0bdc96469
GeorgeWL <georgelangham1990@gmail.com>
2020-06-10 22:39:22 +0100

    add typings to VendingMachine model

--- src/models/vendingMachineModel.ts
+++ src/models/vendingMachineModel.ts
@@ -1,22 +1,33 @@
-import { CoinPropertiesModel, CoinModel } from './coinModels';
+import { CoinModel } from './coinModels';
+import { ICoinProperties } from '../interfaces/coinInterface';
+import ProductModel from './productModel';
 
 export default class VendingMachine {
-  private rejectedCoins: CoinPropertiesModel[] = [];
   private acceptedCoins: CoinModel[] = [];
-  private products: any[] = [];//leave as is for now
+  private rejectedCoins: ICoinProperties[] = [];
+  private products: ProductModel[] = [];
   private displayedMessage: string = 'INSERT COIN';
+
   public get DisplayMessage(): string {
     return this.displayedMessage;
   }
+
   public get AcceptedCoins(): CoinModel[] {
     return this.acceptedCoins;
   }
-  public get Products(): any[] {
+
+  public get RejectedCoins(): ICoinProperties[] {
+    return this.rejectedCoins;
+  }
+
+  public get Products(): ProductModel[] {
     return this.products;
   }
+
   public setDisplayMessage = (newMsg: string) => {
     this.displayedMessage = newMsg;
   }
+
   public setAcceptedCoins = (accepted: CoinModel[]) => {
     this.acceptedCoins = accepted;
   }
