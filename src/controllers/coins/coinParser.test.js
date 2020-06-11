85e39befc5b224be8b7cf4d79b4bcf4248d9dba0
GeorgeWL <georgelangham1990@gmail.com>
2020-06-11 20:20:11 +0100

    coinparser test

--- src/controllers/coins/coinParser.test.js
+++ src/controllers/coins/coinParser.test.js
@@ -1,5 +1,5 @@
-import { getCoinDenomination } from './coinParser';
-import { COIN_DIME, COIN_NICKEL, COIN_QUARTER } from '../../models/coinModels';
+import { getCoinDenomination, getAcceptedAndRejected } from './coinParser';
+import { COIN_DIME, COIN_NICKEL, COIN_QUARTER, DEFAULT_VALID_COINS } from '../../models/coinModels';
 describe('getCoinDenomination tests', () => {
   it('should return coin object from nickel coin properties', () => {
     expect(getCoinDenomination(COIN_NICKEL.CoinProperties).CoinName).toEqual(COIN_NICKEL.CoinName);
@@ -17,12 +17,31 @@ describe('getCoinDenomination tests', () => {
     expect(getCoinDenomination(COIN_QUARTER.CoinProperties).CoinProperties).toEqual(COIN_QUARTER.CoinProperties);
   });
   it('should throw error if invalid coin properties', () => {
-    expect(() => getCoinDenomination({ weightGrams: 50, sizeMillimetres: 50 })).toThrow();
+    expect(() => getCoinDenomination({ weightGrams: 50, diameterMillimetres: 50 })).toThrow();
   });
 });
 
 describe('parse rejected and accepted', () => {
-  it.todo('returns original coin array parsed to denominations inside of accepted value of object if all valid');
-  it.todo('returns original unparsed coin array inside of rejected value of object if all invalid');
-  it.todo('if some valid and some invalid, returns parsed valid coins in accepted, and original unparsed invalid in rejected');
+  it('returns original coin array parsed to denominations inside of accepted value of object if all valid', () => {
+    const coins = DEFAULT_VALID_COINS.map(coin => coin.CoinProperties);
+    expect(getAcceptedAndRejected(coins)).toEqual({ accepted: DEFAULT_VALID_COINS, rejected: [] });
+  });
+  it('returns original unparsed coin array inside of rejected value of object if all invalid but still coins', () => {
+    const coins = [{ weightGrams: 10, diameterMillimetres: 10 }, { weightGrams: .1, diameterMillimetres: .1 }, { weightGrams: 100, diameterMillimetres: 100 }];
+    expect(getAcceptedAndRejected(coins)).toEqual({ accepted: [], rejected: coins });
+
+  });
+  it('returns parsed as undefined values coin array inside of rejected value of object if all invalid and not coins', () => {
+    const coins = [undefined, 'dime', false];
+    const expected = coins.map(() => ({
+      diameterMillimetres: undefined,
+      weightGrams: undefined
+    }));
+    expect(getAcceptedAndRejected(coins)).toEqual({ accepted: [], rejected: expected });
+  });
+  it('if some valid and some invalid, returns parsed valid coins in accepted, and original unparsed invalid in rejected', () => {
+    const accepted = DEFAULT_VALID_COINS.map(coin => coin.CoinProperties);
+    const rejected = [{ weightGrams: 10, diameterMillimetres: 10 }, { weightGrams: .1, diameterMillimetres: .1 }, { weightGrams: 100, diameterMillimetres: 100 }];
+    expect(getAcceptedAndRejected([...accepted, ...rejected])).toEqual({ accepted: DEFAULT_VALID_COINS, rejected });
+  });
 });
--- src/controllers/coins/coinParser.ts
+++ src/controllers/coins/coinParser.ts
@@ -1,11 +1,28 @@
-import { ICoinProperties } from '../../interfaces/coinInterface';
+import { ICoinProperties, ICoinsAcceptedRejected } from '../../interfaces/coinInterface';
 import { CoinModel, DEFAULT_VALID_COINS } from '../../models/coinModels';
+import { validateCoin } from './validateCoins';
 
 export function getCoinDenomination(coin: ICoinProperties, validCoins: CoinModel[] = DEFAULT_VALID_COINS): CoinModel {
-  const coinDenomination: CoinModel | undefined = validCoins.find(validCoin => validCoin.get().weightGrams === coin.weightGrams && validCoin.get().sizeMillimetres === coin.sizeMillimetres);
+  const coinDenomination: CoinModel | undefined = validCoins.find(validCoin => validCoin.CoinWeightGrams === coin.weightGrams && validCoin.CoindiameterMillimetres === coin.diameterMillimetres);
   if (coinDenomination) {
     return coinDenomination;
   } else {
     throw Error('Invalid Coin');
   }
+}
+
+export function getAcceptedAndRejected(coins: ICoinProperties[], validCoins: CoinModel[] = DEFAULT_VALID_COINS): ICoinsAcceptedRejected {
+  const accepted: CoinModel[] = [];
+  const rejected: ICoinProperties[] = [];
+  const coinValidityMap: ICoinProperties[] = coins.map(coin => ({ ...coin, valid: validateCoin(coin, validCoins) }));
+  coinValidityMap.forEach(coin => {
+    if (coin.valid) {
+      accepted.push(getCoinDenomination(coin));
+    } else {
+      rejected.push(
+        { weightGrams: coin?.weightGrams ?? undefined, diameterMillimetres: coin?.diameterMillimetres ?? undefined }
+      );
+    }
+  });
+  return { accepted, rejected };
 }
