ec5639224d8ba8e02d7b94302779b04563fc2ad5
GeorgeWL <georgelangham1990@gmail.com>
2020-06-11 21:38:03 +0100

    update coin model/interface

--- src/interfaces/coinInterface.ts
+++ src/interfaces/coinInterface.ts
@@ -1,15 +1,24 @@
+import { CoinModel } from "../models/coinModels";
+
 // NOTE: Though there's also cent, half dollar and dollar, gonna assume machine only accepts these denominations
 export enum EnumValidCoinTypes {
   'nickel',
   'dime',
-  'quarter'
+  'quarter',
+  'test-float'
 }
 
 export interface ICoinProperties {
-  weightGrams: number;
-  sizeMillimetres: number;
+  weightGrams: number | undefined;
+  diameterMillimetres: number | undefined;
+  valid?: boolean;
 }
 export interface ICoin extends ICoinProperties {
   enumDenomination: EnumValidCoinTypes;
   value: number;
+}
+
+export interface ICoinsAcceptedRejected {
+  accepted: CoinModel[];
+  rejected: ICoinProperties[]
 }
--- src/models/coinModels.ts
+++ src/models/coinModels.ts
@@ -1,10 +1,10 @@
 import { ICoin, EnumValidCoinTypes, ICoinProperties } from '../interfaces/coinInterface';
 export class CoinPropertiesModel {
-  public weightGrams: number = 0;
-  public sizeMillimetres: number = 0;
+  public weightGrams: number | undefined;
+  public diameterMillimetres: number | undefined;
   constructor(options: ICoinProperties) {
     this.weightGrams = options.weightGrams;
-    this.sizeMillimetres = options.sizeMillimetres;
+    this.diameterMillimetres = options.diameterMillimetres;
   }
 }
 export class CoinModel extends CoinPropertiesModel {
@@ -14,7 +14,7 @@ export class CoinModel extends CoinPropertiesModel {
   constructor(options: ICoin) {
     super(options);
     this.weightGrams = options.weightGrams;
-    this.sizeMillimetres = options.sizeMillimetres;
+    this.diameterMillimetres = options.diameterMillimetres;
     this.enumDenomination = options.enumDenomination;
     this.value = options.value;
   }
@@ -22,7 +22,7 @@ export class CoinModel extends CoinPropertiesModel {
   public get CoinProperties(): ICoinProperties {
     return {
       weightGrams: this.weightGrams,
-      sizeMillimetres: this.sizeMillimetres,
+      diameterMillimetres: this.diameterMillimetres,
     };
   }
 
@@ -34,6 +34,14 @@ export class CoinModel extends CoinPropertiesModel {
     return this.value;
   }
 
+  public get CoinWeightGrams(): number {
+    return this.weightGrams;
+  }
+
+  public get CoindiameterMillimetres(): number {
+    return this.diameterMillimetres;
+  }
+
   public get(): ICoin {
     return {
       ...this.CoinProperties,
@@ -42,7 +50,7 @@ export class CoinModel extends CoinPropertiesModel {
     };
   }
 }
-export const COIN_NICKEL = new CoinModel({ value: 0.05, weightGrams: 5.0, sizeMillimetres: 21.21, enumDenomination: EnumValidCoinTypes.nickel });
-export const COIN_DIME = new CoinModel({ value: 0.10, weightGrams: 2.268, sizeMillimetres: 17.91, enumDenomination: EnumValidCoinTypes.dime });
-export const COIN_QUARTER = new CoinModel({ value: 0.25, weightGrams: 5.670, sizeMillimetres: 24.26, enumDenomination: EnumValidCoinTypes.quarter });
+export const COIN_NICKEL = new CoinModel({ value: 0.05, weightGrams: 5.0, diameterMillimetres: 21.21, enumDenomination: EnumValidCoinTypes.nickel });
+export const COIN_DIME = new CoinModel({ value: 0.10, weightGrams: 2.268, diameterMillimetres: 17.91, enumDenomination: EnumValidCoinTypes.dime });
+export const COIN_QUARTER = new CoinModel({ value: 0.25, weightGrams: 5.670, diameterMillimetres: 24.26, enumDenomination: EnumValidCoinTypes.quarter });
 export const DEFAULT_VALID_COINS = [COIN_NICKEL, COIN_QUARTER, COIN_DIME];
