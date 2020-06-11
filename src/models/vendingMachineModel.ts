91fa6a6f49785882e4fd7d6219393eb832747204
GeorgeWL <georgelangham1990@gmail.com>
2020-06-11 21:38:23 +0100

    create vending machine model

--- src/models/vendingMachineModel.ts
+++ src/models/vendingMachineModel.ts
@@ -1,12 +1,22 @@
 import { CoinModel } from './coinModels';
 import { ICoinProperties } from '../interfaces/coinInterface';
 import ProductModel from './productModel';
+import { getCurrentBalanceFromCoins } from '../controllers/machine/balanceParser';
 
 export default class VendingMachine {
   private acceptedCoins: CoinModel[] = [];
   private rejectedCoins: ICoinProperties[] = [];
   private products: ProductModel[] = [];
   private displayedMessage: string = 'INSERT COIN';
+  private selectedProduct: ProductModel | undefined;
+  private machineCoins: CoinModel[] = []
+  private machineBalance: number = 0;
+
+  constructor(initialProducts: ProductModel[], initialCoins: CoinModel[]) {
+    this.products = initialProducts;
+    this.machineCoins = initialCoins;
+    this.machineBalance = getCurrentBalanceFromCoins(initialCoins);
+  }
 
   public get DisplayMessage(): string {
     return this.displayedMessage;
@@ -28,6 +38,10 @@ export default class VendingMachine {
     return this.products.filter(product => product.IsInStock);
   }
 
+  public get SelectedProduct(): ProductModel | undefined {
+    return this.selectedProduct;
+  }
+
   public setDisplayMessage = (newMsg: string) => {
     this.displayedMessage = newMsg;
   }
@@ -40,9 +54,14 @@ export default class VendingMachine {
     this.rejectedCoins = rejected;
   }
 
+  public setSelectedProduct = (product: ProductModel) => {
+    this.selectedProduct = product;
+  }
+
   public buttonRefundAction = () => {
     this.acceptedCoins = [];
     this.rejectedCoins = [];
+    this.selectedProduct = undefined;
   }
 
 }
